d826c4978a364c47b6aeb0a34f6e6e37f1dfe2ff
msterhuj <gabin.lanore@gmail.com>
2020-12-12 23:43:45 +0100

    1.2
    replace merger to sqlite database
    update output to exporter function

--- Hashes.orgTools.py
+++ Hashes.orgTools.py
@@ -3,23 +3,26 @@ import requests
 import json
 import os
 from glob import glob
+import sqlite3
+from progress.bar import Bar
 
+database = sqlite3.connect("password.db")
+database_cursor = database.cursor()
+try:
+    database_cursor.execute("create table password(content varchar not null);")
+    print("Created password table on password.db")
+except sqlite3.OperationalError:
+    print("Table already exists [Skipped]")
 
 @click.command()
 @click.option('--download', '-dl', type=bool, is_flag=True, help="Download all leaks wordlist")
-@click.option('--merge', '-m', type=bool, is_flag=True, help="Merge all downloaded file in one file")
-@click.option('--clean', '-c', type=bool, is_flag=True, help="Remove duplicated line")
-@click.option('--output', '-o', help="output file exit")
-def main(download: bool, merge: bool, clean: bool, output: str):
+@click.option('--merge', '-m', type=bool, is_flag=True, help="Merge all downloaded file in password database for export")
+@click.option('--export', '-e', help="export db to a text file")
+def main(download: bool, merge: bool, export: str):
     """
         Download all wordlist on hashes.org and generate one unique file\n
         By @MsterHuj
     """
-
-    if merge or clean:
-        if output is None:
-            print("Error you need to specify an output file (--output <filename>)")
-            exit(1)
 
     if download:
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0",
@@ -50,33 +53,27 @@ def main(download: bool, merge: bool, clean: bool, output: str):
             leak_current += 1
 
     if merge:
-        print("Merging files")
-        with open(output, "wb") as outfile:
-            for url in glob("wordlist/*.txt"):
-                print("Merging " + str(url) + " in " + output)
-                with open(url, "rb") as infile:
-                    outfile.write(infile.read())
+        print("Merging files on database and remove duplicate password")
+        for url in glob("wordlist/*.txt"):
+            name = url.replace("\\", "/").split("/")[1]
+            alist = [line.rstrip() for line in open(url)]
+            bar = Bar(name, max=len(alist))
+            for password in alist:
+                try:
+                    database_cursor.execute("insert into password (content) values ('" + password.replace("'", "''") + "');")
+                    database.commit()
+                except sqlite3.IntegrityError:
+                    pass
+                finally:
+                    bar.next()
+            bar.finish()
 
-    if clean:  # make this function less ram-eating ^^'
-        print("Cleaning file")
-        lines_seen = set()
-        with open("temp_" + output, "w") as output_file:
-            for each_line in open(output, "r"):
-                if each_line not in lines_seen:
-                    output_file.write(each_line)
-                    lines_seen.add(each_line)
-                    print("Removing duplicated line : " + each_line.replace('\n', ''))
-        os.remove(output)
-        os.rename("temp_" + output, output)
-
-    if download is None and merge is None and clean is None and output is None:
-        os.system("python3 Hashes.orgTools.py --help")
-    else:
-        if output is not None:
-            print(str(sum(1 for line in open(output))) + " : passwords in file")
-        print("End of app thanks to using me !")
-        print("Written by @MsterHuj")
-
+    if export:
+        print("Exporting password database")
+        passwords = database_cursor.execute("select * from password;")
+        with open(export, 'w') as f:
+            for item in passwords:
+                f.write("%s\n" % item)
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -3,24 +3,33 @@ Download all wordlist on hashes.org and generate one unique file
 > Need Python3 and pip3
 ## Install
 
-Install lib with pip3
-`pip3 install -r requirements.txt`
+Install lib with pip3 `pip3 install -r requirements.txt`
 
 ````
 Usage: Hashes.orgTools.py [OPTIONS]
 
+  Download all wordlist on hashes.org and generate one unique file
+
+  By @MsterHuj
+
 Options:
   -dl, --download    Download all leaks wordlist
-  -m, --merge        Merge all downloaded file in one file
-  -c, --clean        Remove duplicated line
-  -o, --output TEXT  output file exit
+  -m, --merge        Merge all downloaded file in password database for export
+  -e, --export TEXT  export db to a text file
   --help             Show this message and exit.
 ````
 
 ## Next update
-Download only file updated or added
+ * Download only file updated or added
+ * Random agent for download
+ * Regex check before add to database
 
 ## Change log
+ * 1.2
+   * replace merger to sqlite database
+   * update output to exporter function
+ * 1.1
+   * fix download error from hashes.org
  * 1.0
    * Download all leaks wordlist
    * Merge all downloaded file in one file
